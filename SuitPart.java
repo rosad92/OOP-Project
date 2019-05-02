0c128c62c16e5837b7b9292ed2a8db63fc0be510
rosad92 <49042220+rosad92@users.noreply.github.com>
2019-05-02 16:03:14 -0400

    Update SuitPart.java

--- SuitPart.java
+++ SuitPart.java
@@ -1,1 +1,13 @@
-
+public class SuitPart extends item{
+	String name;
+	SuitPart(String name){
+		this.name = name;
+	}
+	public boolean use(){
+		//this will add to a total count of suitparts on
+		return true;
+	}
+	public void look(){
+		System.out.println(name + "will keep you breathing outside the ship.");
+	}
+}
