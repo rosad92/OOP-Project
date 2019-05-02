e6fb192e14ebb5c79d1ac1c227948eb93280a7e6
jolm97 <49042175+jolm97@users.noreply.github.com>
2019-05-02 15:18:16 -0400

    person.java

---
+++ player.java
@@ -0,0 +1,17 @@
+public class player {
+	scanner myScanner= new Scanner (System.in);
+	System.out.println("Enter Player Name:");
+	Scanner enterScanner = new Scanner (System.in);
+	int playerHP;
+	// moving player code
+	public void move(int row, int column) {
+		this.row = row;
+		this.column= column;
+
+	}
+
+	public static playerSetup() {
+	
+		
+	}
+}
