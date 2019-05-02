77d90fb824c09917bc47e9467099d839c48791b4
rosad92 <49042220+rosad92@users.noreply.github.com>
2019-05-02 19:26:28 -0400

    Update fuel.java

--- fuel.java
+++ fuel.java
@@ -1,1 +1,9 @@
-
+public class fuel extends item{
+	public boolean use(){
+		//if in the right spot, add to fuel meter
+		return true;
+	}
+	public void look(){
+		System.out.println("Fuel can be used to get the ship off the ground")
+	}
+}
