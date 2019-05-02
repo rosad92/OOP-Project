37feaa747093c4d4676f8380641d42cf9af13041
rosad92 <49042220+rosad92@users.noreply.github.com>
2019-05-02 15:58:31 -0400

    Update tool.java

--- tool.java
+++ tool.java
@@ -1,1 +1,12 @@
-
+public class tool{
+	String name;
+	tool(String name){
+		this.name = name;
+	}
+	public boolean use(){
+		//tools will have specific uses
+	}
+	public void look(){
+		System.out.println(name + " can be used to repair the ship.");
+	}
+}
