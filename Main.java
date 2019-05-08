85e5005158728fed06965662c926b8b6988ffbe8
jolm97 <49042175+jolm97@users.noreply.github.com>
2019-05-07 20:09:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,19 @@
 import java.util.Scanner;
 
-public class main{
-	public static void start (String[] args) {
-		Scanner sc = new Scanner(System.in);
+public class Start{
+	public static void main(String[] args) {
 		System.out.println("Mission Home");
+		Scanner input = new Scanner (System.in);
 		System.out.println("Please enter your name");
-		String name= sc.nextLine();
-		System.out.println("Hello Commander" + name);
-		sc.close();
+		String name= input.next();
+		input.close();
 		
 
 		System.out.println("*CRASH* You're an astronaut on the Akash Space Station (A.S.S)"
 				+ "and It has crashed on an unknown planet. Your ship is "
 				+ "damaged. Your new mission, collect the items around the"
 				+ "ship and planet to fix the A.S.S and get back home.");
-		Scanner a= new Scanner(System.in);
-		System.out.println("Hello Commander" + name+ "Let's get home"
-				+"Enter 'Start' to begin the game");
-		String StartGame= a.nextLine();
-		if(StartGame=="Start") {
-			System.out.println("Gald to have your help Commander");
-			a.close();
+		System.out.println("Hello Commander, " + " "+ name +" "+ " Let's go home");
 		}
 	}
-}
+
