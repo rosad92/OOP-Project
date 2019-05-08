7f0f459dbb3d265eafc6b54fed7b308c06bb37f7
rosad92 <49042220+rosad92@users.noreply.github.com>
2019-05-08 19:58:33 -0400

    Update Start.java

--- Start.java
+++ Start.java
@@ -15,12 +15,16 @@ public class Start{
 				+ "damaged. Your new mission, collect the items around the"
 				+ "ship and planet to fix the A.S.S and get back home.");
 		System.out.println("Hello Commander, " + " "+ name +" "+ " Let's go home");
+		String newLine = System.getProperty("line.separator");
+		System.out.println("---------------------------------------------------------------" +newLine);
+		System.out.println("Welcome to the A.S.S Commander. Upon impact we have lost multiple items vital"
+				+ newLine
+				+" to our ship such as, tools, fuel"
+				+ "food, spacesuit parts, ship parts and flashlights."
+				+ newLine	+"Your mission is to gather the missing parts from the ship"+ newLine
+				+ "and the planet and return them to the A.S.S to use to get us home.");
 		
-<<<<<<< HEAD
-		// tiny bit more explanation
-		//tiny bit more explanation
-=======
->>>>>>> 158e0232f831028cacf228453fe4acb42dc83b56
+
 		//print about starting room
 		
 		do{
