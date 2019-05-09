d2b713a6515cf040a1c247c96bf88e54142e0c1e
rosad92 <49042220+rosad92@users.noreply.github.com>
2019-05-09 14:01:32 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -7,15 +7,32 @@ public class Player {
 		inventory = new item [17];
 		
 		//command center item
-		inventory [0] = new tool("hammer");
+		inventory [0] = new tool("flashlight");
 		//sleeping quarters items
-		inventory [1] = new tool("flashlight");
+		inventory [1] = new tool("hammer");
 		inventory[2] = new SuitPart("helmet");
 		//communal area
-		inventory[3] = new SuitPart("gloves");
+		inventory[3] = new SuitPart("pants");
 		inventory[4] = new food("apple");
+		//cargo bay
+		inventory[5] = new ShipParts("screws");
+		inventory[6] = new food("bread");
 		//engine
-		inventory[5] = new fuel();
+		inventory[7] = new fuel();
+		//airlock
+		inventory[8] = new SuitPart("jacket");
+		//crater
+		inventory[9] = new ShipParts("wing");
+		inventory[10] = new tool("screwdriver");
+		//dunes
+		inventory[11] = new tool("wire");
+		inventory[12] = new ShipParts("wheels");
+		//cave
+		inventory[13] = new ShipParts("rudder");
+		inventory[14] = new tool("tape");
+		//shelter
+		inventory[15] = new fuel();
+		inventory[16] = new food("preserved food");
 		
 	}
 	public String getName() {
