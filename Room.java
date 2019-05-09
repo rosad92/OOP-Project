02b7a27ee8bc3b4edfb7f2df0fa7185055f6a4a0
As2830 <49209932+As2830@users.noreply.github.com>
2019-05-08 20:36:22 -0400

    Update and rename ItemPlacement.java to Room.java

---
+++ Room.java
@@ -0,0 +1,29 @@
+
+
+public class ItemPlacement {
+	
+	String roomname;
+	boolean hasitem;
+	boolean visited;
+	
+	public String getroomname() {
+		return roomname;
+	}
+	
+	public boolean gethasitem() {
+		return hasitem;
+	}
+	
+	public boolean getvisited() {
+		return visited; 
+	}
+	
+	public void setroomname (String roomname){
+		this.roomname = roomname;
+	}
+	public void sethasitem (boolean hasitem){
+		this.hasitem = hasitem;
+	}
+	public void setvisited (boolean visited){
+		this.visited = visited;		
+}
