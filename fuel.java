f76acab7a04fa2cafb4940996329ed12d47caf37
As2830 <49209932+As2830@users.noreply.github.com>
2019-05-09 16:23:43 -0400

    Update fuel.java

--- fuel.java
+++ fuel.java
@@ -5,11 +5,29 @@ public class fuel extends item{
 		hasFuel = false;
 		hasUsed =false;
 	}
+	public boolean gethasFuel() {
+		return hasFuel;
+	}
+	public boolean gethasUsed() {
+		return hasUsed;
+	}
+	public void sethasUsed (boolean hasUsed){
+		this.hasUsed = hasUsed;		
+        }
+	public void sethasFuel (boolean hasFuel){
+		this.hasFuel = Fuel;		
+        }
 	public boolean use(){
+		if(hasUsed == true){
+			return false;
+		}
+		if(hasUsed == false){
+		        hasUsed = true;
+			return true;	
+		}
 		//similar to food
 		//if it hasUsed is true, return false
-		//if hasUsed is false, change to true, return false
-		return true;
+		//if hasUsed is false, change to true, return false <----is this supposed to say true like in food? 
 	}
 	public void pickup(){
 		if (hasFuel == false){
