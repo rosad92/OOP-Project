c23d78966df1669ce36d4b8769dc1ea1c3232d91
As2830 <49209932+As2830@users.noreply.github.com>
2019-05-09 15:46:44 -0400

    Update food.java

--- food.java
+++ food.java
@@ -7,10 +7,33 @@ public class food extends item{
 		hasFood = false;
 		hasUsed= false;
 	}
+	public String getname (){
+		return name;
+	}
+	public boolean gethasFood() {
+		return haspart;
+	}
+	public boolean gethasUsed() {
+		return hasUsed;
+	}
+	public void setname (String name){
+		this.name = name;
+	}
+	public void sethasFood (boolean hasPart){
+		this.hasPart = hasPart;
+	}
+	public void sethasUsed (boolean hasUsed){
+		this.hasUsed = hasUsed;		
 	public boolean use(){
+		if(hasUsed == true) {
+			return false;
+		}
+		if(hasUsed == false) {
+			hasUsed = true;
+			return true;
+			
 		//if hasUsed is true, return false
 		//if hasUsed is false, change hasUsed to true, return true
-		return true;
 	}
 	public void pickup(){
 		if(hasFood == false){
