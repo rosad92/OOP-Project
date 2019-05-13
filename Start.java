89f22c09ce140da4713e200059b4d84b563a20f4
rosad92 <49042220+rosad92@users.noreply.github.com>
2019-05-13 19:41:41 -0400

    Update Start.java

--- Start.java
+++ Start.java
@@ -11,19 +11,19 @@ public class Start{
 		
 		
 
-		System.out.println("*CRASH* You're an astronaut on the Akash Space Station (A.S.S)"
+		System.out.println("*CRASH* You're an astronaut on the Akash Space Station (AK.S.S)"
 				+ "and It has crashed on an unknown planet. Your ship is "
 				+ "damaged. Your new mission, collect the items around the"
-				+ "ship and planet to fix the A.S.S and get back home.");
+				+ "ship and planet to fix the AK.S.S and get back home.");
 		System.out.println("Hello Commander, " + " "+ name +" "+ " Let's go home");
 		String newLine = System.getProperty("line.separator");
 		System.out.println("---------------------------------------------------------------" +newLine);
-		System.out.println("Welcome to the A.S.S Commander. Upon impact we have lost multiple items vital"
+		System.out.println("Welcome to the AK.S.S Commander. Upon impact we have lost multiple items vital"
 				+ newLine
 				+" to our ship such as, tools, fuel"
 				+ "food, spacesuit parts, ship parts and flashlights."
 				+ newLine	+"Your mission is to gather the missing parts from the ship"+ newLine
-				+ "and the planet and return them to the A.S.S to use to get us home.");
+				+ "and the planet and return them to the AK.S.S to use to get us home.");
 		
 
 		//print about starting room
